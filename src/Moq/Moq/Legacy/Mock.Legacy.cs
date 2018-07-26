05dae8d90f4907a72f2e57466ee177c1556df523
Steffen Loesch <stloesch@microsoft.com>
2018-07-25 17:45:49 -0700

    Add comments to legacy Mock class.

--- src/Moq/Moq/Legacy/Mock.Legacy.cs
+++ src/Moq/Moq/Legacy/Mock.Legacy.cs
@@ -1,7 +1,13 @@
 using System;
 
+/// <summary>
+/// Legacy class to provide custom error and codefix support.
+/// </summary>
 public class Mock<T> where T : class
 {
+    /// <summary>
+    /// Legacy constructor to support portability to new API.
+    /// </summary>
     [Obsolete("Please run the Moq migration code fix.", true)]
     public Mock()
     {
