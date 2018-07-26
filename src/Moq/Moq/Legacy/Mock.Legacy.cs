3e42ab082b6d0372137d7a32db1e26a546e1876d
Steffen Loesch <stloesch@microsoft.com>
2018-07-25 22:45:11 -0700

    Add namespace to legacy Mock class.

--- src/Moq/Moq/Legacy/Mock.Legacy.cs
+++ src/Moq/Moq/Legacy/Mock.Legacy.cs
@@ -1,16 +1,19 @@
 using System;
 
-/// <summary>
-/// Legacy class to provide custom error and codefix support.
-/// </summary>
-public class Mock<T> where T : class
+namespace Moq
 {
     /// <summary>
-    /// Legacy constructor to support portability to new API.
+    /// Legacy class to provide custom error and codefix support.
     /// </summary>
-    [Obsolete("Please run the Moq migration code fix.", true)]
-    public Mock()
+    public class Mock<T> where T : class
     {
-        throw new NotImplementedException("This Moq4 API is not supported any more. Please run the Moq migration code fix.");
+        /// <summary>
+        /// Legacy constructor to support portability to new API.
+        /// </summary>
+        [Obsolete("Please run the Moq migration code fix.", true)]
+        public Mock()
+        {
+            throw new NotImplementedException("This Moq4 API is not supported any more. Please run the Moq migration code fix.");
+        }
     }
 }
