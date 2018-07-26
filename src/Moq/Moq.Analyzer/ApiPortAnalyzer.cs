8302b7aba4392be268369e3a26b26aac7847beca
Steffen Loesch <stloesch@microsoft.com>
2018-07-25 17:46:15 -0700

    Add stub of analyzer for porting APIs.

---
+++ src/Moq/Moq.Analyzer/ApiPortAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Moq
+{
+    /// <summary>
+    /// Generates diagnostics for using unsupported Moq APIs.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class ApiPortAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+
+        public override void Initialize(AnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
