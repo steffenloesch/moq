78caba95821ecf3d1f63a1886f8f63753549ebe1
Steffen Loesch <stloesch@microsoft.com>
2018-07-25 22:48:08 -0700

    Start with analyzer implementation.

--- src/Moq/Moq.Analyzer/ApiPortAnalyzer.cs
+++ src/Moq/Moq.Analyzer/ApiPortAnalyzer.cs
@@ -11,13 +11,25 @@ namespace Moq
     [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
     public class ApiPortAnalyzer : DiagnosticAnalyzer
     {
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+        private const string mockMetadataName = "Mock`1";
+
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
             => ImmutableArray.Create(MockDiagnostics.ObsoleteApi);
 
         public override void Initialize(AnalysisContext context)
         {
-            throw new NotImplementedException();
+            context.RegisterSyntaxNodeAction(AnalyzeObjectCreation, Microsoft.CodeAnalysis.CSharp.SyntaxKind.ObjectCreationExpression);
+            context.RegisterSyntaxNodeAction(AnalyzeObjectCreation, Microsoft.CodeAnalysis.VisualBasic.SyntaxKind.ObjectCreationExpression);
+        }
+
+        void AnalyzeObjectCreation(SyntaxNodeAnalysisContext context)
+        {
+            // Get the type symbol for Mock<T>.
+            var genericMockType = context.Compilation.GetTypeByMetadataName(mockMetadataName);
+            if (genericMockType == null)
+                return;
+
+            //TODO: continue implementation
         }
     }
 }
